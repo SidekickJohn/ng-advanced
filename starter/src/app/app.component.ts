973d632208aa627cf7b983275e0013d25908dc7d
SidekickJohn <sidekick.john@gmail.com>
2017-12-04 13:59:43 +0100

    Loading indicator --> stays if candeactivate guard is present

--- starter/src/app/app.component.ts
+++ starter/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router} from "@angular/router";
 
 @Component({
   selector: 'flight-app',
@@ -6,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent  {
-  title = 'Manfred war hier!';
+  showLoadingIndicator: boolean = false;
+
+  constructor(private router: Router) {
+    router.events
+      .filter( e => e instanceof NavigationStart)
+      .subscribe(event => {
+        this.showLoadingIndicator = true;
+    });
+
+    router.events
+      .filter( e => e instanceof NavigationEnd
+                || e instanceof NavigationError
+                || e instanceof NavigationCancel)
+      .subscribe(event => {
+        this.showLoadingIndicator = false;
+      });
+  }
+
 }
 
